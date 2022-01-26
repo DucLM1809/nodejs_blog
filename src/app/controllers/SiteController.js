cf843f8d72f5a52e8d0ae50d06640c8ce887fa6d
duc minh <minhductxqt@gmail.com>
2022-01-26 15:58:24 +0700

    sort function

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,21 @@
 const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
 class SiteController {
     // [GET] /
-    index(req, res) {
-        Course.find({}, function (err, courses) {
-            if (!err) res.json(courses)
-            res.status(400).json({ error: 'ERROR!!!' })
-        })
-        // res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
     }
 
     // [GET] /search
     search(req, res) {
-        res.render('search');
+        res.render('search')
     }
 }
 
-module.exports = new SiteController();
+module.exports = new SiteController()
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const express = require('express')
+const methodOverride = require('method-override')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
-
 const route = require('./routes')
 const db = require('./config/db')
+const SortMiddleware = require('./app/middlewares/sortMiddleware')
 
 // Connect to DB
 db.connect()
@@ -19,12 +20,42 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
+app.use(methodOverride('_method'))
+
+// Custom Middleware
+app.use(SortMiddleware)
+
 // HTTP logger
 app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
+  extname: '.hbs',
+  helpers: {
+    sum: (a, b) => a + b,
+    sortable: (field, sort) => {
+      const sortType = field === sort.column ? sort.type : 'default'
+
+      const icons = {
+        default: 'oi oi-elevator',
+        asc: 'oi oi-sort-ascending',
+        desc: 'oi oi-sort-descending',
+      }
+
+      const types = {
+        default: 'desc',
+        asc: 'desc',
+        desc: 'asc',
+      }
+
+      const icon = icons[sortType]
+      const type = types[sortType]
+
+      return `<a href="?_sort&column=${field}&type=${type}">
+          <span class="${icon}"></span>
+        </a>`
+    }
+  }
 }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'))
@@ -33,5 +64,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'))
 route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 }) 
